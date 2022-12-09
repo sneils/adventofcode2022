961adbe8442eaad94b870121911d341a56353340
Franz B <franz.buchsbaum@kununu.com>
2022-12-09 17:04:44 +0100

    Reimplement day9 for part2

--- solutions/9.py
+++ solutions/9.py
@@ -1,17 +1,18 @@
 class Knot:
     def __init__(self):
         self.x, self.y = 0, 0
+        self.next = None
         self.visits = set()
         self.visit()
 
-    def get(self):
-        return self.x, self.y
+#    def get(self):
+#        return self.x, self.y
+#
+#    def visited(self, x, y):
+#        return "{},{}".format(x, y) in self.visits
 
     def visit(self):
         self.visits.add("{},{}".format(self.x, self.y))
-
-    def visited(self, x, y):
-        return "{},{}".format(x, y) in self.visits
 
     def move(self, direction):
         match direction:
@@ -27,66 +28,65 @@ class Knot:
                 raise ValueError
 
         self.visit()
+        self.pull()
 
-    def pull(self, to):
-        dx, dy = to.x-self.x, to.y-self.y
+    def pull(self):
+        if self.next == None:
+            return
+
+        dx, dy = self.x-self.next.x, self.y-self.next.y
         if abs(dx) > 1 or abs(dy) > 1:
-            self.x += max(-1, min(1, dx))
-            self.y += max(-1, min(1, dy))
+            self.next.x += max(-1, min(1, dx))
+            self.next.y += max(-1, min(1, dy))
 
-        self.visit()
+        self.next.pull()
+        self.next.visit()
 
 
 class Rope:
     def __init__(self, n):
-        self.parts = [Knot() for _ in range(n)]
-
-    def head(self):
-        return self.parts[0]
-
-    def tail(self):
-        return self.parts[-1]
+        self.head = knot = Knot()
+        for _ in range(1, n):
+            knot.next = Knot()
+            knot = knot.next
+        self.tail = knot
 
     def move(self, direction, distance):
         for _ in range(distance):
-            self.head().move(direction)
-            self.pull()
-
-    def pull(self):
-        for i in range(1, len(self.parts)):
-            last, curr = self.parts[i-1:i+1]
-            curr.pull(last)
+            self.head.move(direction)
 
 
 def parse(line):
     return [t(s) for t, s in zip((str, int), line.split())]
 
 
-def print(rope, mark_path=False):
-    d = 5  # d=200
-    for y in range(d, -d-1, -1):
-        for x in range(-d, d+1, +1):
-            if (x, y) == rope.head().get():
-                print("H", end="")
-            elif (x, y) == rope.tail().get():
-                print("T", end="")
-            elif (x, y) == (0, 0):
-                print("s", end="")
-            elif mark_path and rope.tail().visited(x, y):
-                print("x", end="")
-            else:
-                print(".", end="")
-
-        print()
-
-    print()
+# def draw(rope, mark_path=False):
+#    d = 5  # d=200
+#    for y in range(d, -d-1, -1):
+#        for x in range(-d, d+1, +1):
+#            if (x, y) == rope.head.get():
+#                print("H", end="")
+#            elif (x, y) == rope.tail.get():
+#                print("T", end="")
+#            elif (x, y) == (0, 0):
+#                print("s", end="")
+#            elif mark_path and rope.tail.visited(x, y):
+#                print("x", end="")
+#            else:
+#                print(".", end="")
+#
+#        print()
+#
+#    print()
 
 
 def run(data):
-    rope = Rope(2)
+    ropes = (Rope(2), Rope(10))
 
     for line in data:
         d, n = parse(line)
-        rope.move(d, n)
+        for rope in ropes:
+            rope.move(d, n)
 
-    return len(rope.tail().visits), 0
+    p1, p2 = [len(rope.tail.visits) for rope in ropes]
+    return p1, p2
